bb5c74bb1f6192fd9ff0844aef274ab23a07605b
User <User@Lenovo-PC.local>
2018-03-22 17:33:32 +0100

    pridana reprezentacia beykontextovej gramatiky

---
+++ src/grammar/ContextFreeGrammar.java
@@ -0,0 +1,25 @@
+package grammar;
+
+import java.util.HashSet;
+
+public class ContextFreeGrammar extends Grammar {
+
+	public ContextFreeGrammar(HashSet<String> terminals, HashSet<String> nonterminals, HashSet<Rule> rules,
+			String startsymbol) throws Exception {
+		super(terminals, nonterminals, rules, startsymbol);
+		// TODO Auto-generated constructor stub
+		int check=0;					//checks if left side of rule is just one symbol
+		for(Rule r : rules) {
+			if(r.getLeftSide().size()==1) {			
+				check=check+1;
+			}
+		}
+		if(check!=rules.size()) {
+			throw new Exception("Left side of rule has to be just one nonterminal");
+		}
+		else {
+			
+		}
+	}
+
+}
